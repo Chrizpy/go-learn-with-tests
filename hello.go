9a3e640f88da10101f1935fa34cbbcef07bdc384
Chris <christofferhaj@gmail.com>
2021-09-10 15:03:27 +0200

    One last refactor

--- hello.go
+++ hello.go
@@ -14,16 +14,20 @@ func Hello(name string, language string) string {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + exclamationMarkSuffix
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name + exclamationMarkSuffix
+	return
 }
 
 func main() {
